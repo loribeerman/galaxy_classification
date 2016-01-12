9328c259fa9c755804e9614207b88f65343e1353
Lori Beerman <beermalc@astro.washington.edu>
2016-01-12 09:40:47 -0800

    added keras_NN.py

---
+++ code/keras_NN.py
@@ -0,0 +1,33 @@
+import numpy as numpy
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+import os
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.optimizers import SGD, RMSprop, Adagrad, Adam, Adadelta
+from keras.utils import np_utils
+from sklearn.cross_validation import train_test_split
+from scipy import misc
+from sklearn.metrics import roc_auc_score, roc_curve, classification_report, confusion_matrix
+import read_images
+import get_labels
+import Metrics
+import Plots
+
+'''create neural net with keras and fit it to data (use final model)
+output predictions for input data'''
+
+
+if __name__ == '__main__':
+    data = np.loadtxt('gal_pos_label.txt', delimiter=',')  	# columns are:  dr7objid,ra,dec,spiral,elliptical,uncertain 
+    gal_id = np.genfromtxt('gal_pos_label.txt', delimiter=',', dtype=int, usecols=[0])   
+    spiral = data[:,3]
+    elliptical = data[:,4]
+    uncertain = data[:,5]
+
+    # take subset
+    num_images = 20000
+    gal_set20 = gal_id[0:num_images]
+    # only take center 10000 pixels
+    num_pix_cen = 10000
+    make_img_arr(gal_set20, num_pix_cen, pix_func=read_images.get_pix_flat_cen)
