c06cb07f5d53677047df39a3b5a8e3479729a4eb
Lori Beerman <loribeermanpayne@gmail.com>
2016-04-12 09:08:01 -0700

    fixed indentation

--- code/bg_subtract.py
+++ code/bg_subtract.py
@@ -4,12 +4,12 @@ import numpy as np
 
 
 def bg_subtract(img_arr):
-	'''subtract average of background (10 pixels at each corner) from 1D image array'''
+    '''subtract average of background (10 pixels at each corner) from 1D image array'''
 
-	img_arr_bg = np.zeros((img_arr.shape[0], img_arr.shape[1]))
-	bg = np.zeros(img_arr.shape[0])
-	for i in range(img_arr.shape[0]):
-    	bg[i] = np.mean((img_arr[i][0:10], img_arr[i][190:200], img_arr[i][39800:39810], img_arr[i][39990:40000]))
-    	img_arr_bg[i] = img_arr[i] - bg[i]
+    img_arr_bg = np.zeros((img_arr.shape[0], img_arr.shape[1]))
+    bg = np.zeros(img_arr.shape[0])
+    for i in range(img_arr.shape[0]):
+        bg[i] = np.mean((img_arr[i][0:10], img_arr[i][190:200], img_arr[i][39800:39810], img_arr[i][39990:40000]))
+        img_arr_bg[i] = img_arr[i] - bg[i]
 
     return img_arr_bg
